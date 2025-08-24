9ff444e187b795e5fd1594aca0d8159341784d4b
Kuldeep <hi@kuldeep.tech>
2025-08-24 06:31:15 +0530

    Update .env.example to change DATABASE_CONNECTOR from 'neon' to 'pg' and modify auth-client.ts to dynamically set baseURL and basePath for the authentication client based on the server URL.

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -2,8 +2,11 @@ import { adminClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 import { ac, roles } from "server/src/lib/permission";
 
+const parsedUrl = new URL(import.meta.env.VITE_SERVER_URL);
+const pathname = parsedUrl.pathname;
 export const authClient = createAuthClient({
-  baseURL: import.meta.env.VITE_SERVER_URL,
+  baseURL: parsedUrl.origin,
+  basePath: pathname !== "/" ? `${pathname}/api/auth` : "/api/auth",
   plugins: [
     adminClient({
       // biome-ignore lint/suspicious/noExplicitAny: <>
